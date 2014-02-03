4b904f5236489312b94b71d4524117c9d7599412
Franciny <francinyazevedo@FrancinyAzevedo.(none)>
2014-02-03 20:19:53 -0200

    Criação do Projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hotelaria::Application.config.secret_key_base = 'a49d95e8369d1285772082d8998daeeb6bd13b65290ca9b73e5208bad6d456126dd8b6ffb946d6dbd048b7c18ddf04ca9b1e1a0b9b426214fbae9e052d92c21c'
